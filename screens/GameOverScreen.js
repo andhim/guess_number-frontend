67b762dfef965c006533b17b3a696d7e38b2ab25
Jaeyoung Park <andhim@Jaeyoungs-MacBook-Pro.local>
2021-07-16 18:42:40 -0600

    axios

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,7 +1,17 @@
+import axios from 'axios';
 import React from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
 
 const GameOverScreen = props => {
+
+    const putDataIntoBackend = async() => {
+        const response = await axios("http://localhost:8080/postNumbers", {
+            userNum: props.userNum,
+            roundNum: props.roundNum
+        });
+        console.log(response.data);
+    };
+
     return (
         <View style={styles.screen}>
             <Text>The Game is over</Text>
